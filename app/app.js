d961860714b6e99e7396946ac013c2c2d76c9abd
null <null>
2023-07-17 09:25:47 +0000

    updated port reference and commands

--- app/app.js
+++ app/app.js
@@ -17,9 +17,9 @@ var app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello from Express.js (Node.js) on OpenShift!');
-  
+
 });
 
-app.listen(3000, function () {
+app.listen(8080, function () {
   console.log('Example app listening on port 3000!');
 });
